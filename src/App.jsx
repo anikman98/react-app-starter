39146828ad39c95685a826e405218134fad63252
anikman <anikman98@gmail.com>
2025-01-22 01:40:08 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more with Anik Mandal
+        Click on the Vite and React logos to learn more with Anik Mandal and others
       </p>
     </>
   )
